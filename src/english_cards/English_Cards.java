052ef3489400487c06e8e2ee283219fc6710cdb0
aly <>
2025-12-11 16:27:21 -0500

    prueba

--- src/english_cards/English_Cards.java
+++ src/english_cards/English_Cards.java
@@ -5,6 +5,7 @@ public class English_Cards {
     public static void main(String[] args) {
         System.out.println("Dios mio, ojala");
         System.out.println("hola");
+        //hey
     }
     
 }
