91f685cf3cd6bf03a94a1aa787bceb07c695e832
albaashley57-rgb <albaashley57@gmail.com>
2025-12-19 10:59:05 -0500

    modos de juego(1)

--- src/english_cards/PracticeMode.java
+++ src/english_cards/PracticeMode.java
@@ -4,6 +4,6 @@ import java.util.Scanner;
 
 public interface PracticeMode {
     boolean supports(Card card);
-    void play(Card card, Scanner sc, ScoreTracker score);//ponerle al score el nombre del tracker
+    void play(Card card, Scanner sc, ScoreTracker score);
     
 }
